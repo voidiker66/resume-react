494b0ba0a38f9ccf575b2be464825d87d49d1a36
voidiker66 <asi327@utexas.edu>
2018-08-02 19:20:25 -0700

    work experience page searches for projects with same work id, but projects not loaded correctly

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Pagination from './pagination';
+import {Col} from 'react-bootstrap';
 import Background from './Background';
 
 const API_URL = "http://localhost:5000/api/work/";
@@ -7,10 +10,14 @@ export default class Experience extends React.Component {
 
   constructor() {
     super();
+
+    this.onChangePage = this.onChangePage.bind(this);
    
     this.state = {
         work: [],
         company: [],
+        project: [],
+        pageOfItems: []
     };
   }
 
@@ -24,28 +31,57 @@ componentDidMount() {
   })
     });
   fetch("http://localhost:5000/api/company")
+  .then(re => {
+    return re.json();
+  }).then(da => {
+    
+    this.setState({
+      company: da.objects
+    })
+  });
+  fetch("http://localhost:5000/api/project?q={\"filters\":[{\"name\":\"project_work\",\"op\":\"eq\",\"val\":" + this.props.match.params.work_id + "}]}")
   .then(r => {
     return r.json();
   }).then(d => {
-    
     this.setState({
-      company: d.objects
+      project: d.objects
     })
-  });
+  }); 
+}
+
+onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
 }
 
   render() {
+    var cardStyle = {
+    display: 'block',
+    transitionDuration: '0.3s',
+    height: '15vw',
+    overflow: 'hidden'
+  }
+  const projectItems =this.state.pageOfItems.map(item => {
+    <Link to={"/project/"+item.project_db_id}>
+    <Col key ={item.project_db_id} xs={6} md={4}>
+
+      <div className="card grid" style={cardStyle}>
+        <p className='card-name important'><b>{item.project_name}</b></p>
+        <p className='card-name'>{item.project_languages}</p>
+          <p className='card-name' dangerouslySetInnerHTML={{ __html: item.project_description}}></p>
+      </div>
+    </Col>
+    </Link>
+}
+    );
     return (
     	<div>
       <Background/>
-        <h1>{this.state.company.map((c, index) => {
-          if (c.company_db_id === this.state.work.work_company) {
-            this.state.company = c;
-            return c.company_name;
-          }
-        })}</h1>
+
         <h3>{this.state.work.work_job_title}</h3>
         <p dangerouslySetInnerHTML={{ __html: this.state.work.work_description}}></p>
+        {projectItems}
+        <Pagination items={this.state.project} onChangePage={this.onChangePage}/>
 		  </div>
     );
   }
